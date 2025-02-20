a7e78093c1a48ef66ed75ce10280da22a7611f3d
medamap <junmera@gmail.com>
2025-02-20 12:27:46 +0900

    Improve UDP client broadcast handling on Android
    - Detect broadcast addresses in SocketUdpClient.Connect() and avoid using Connect() for broadcast.
    - Enable SocketOptionName.Broadcast when a broadcast address is used.
    - Modify SendAsync() to use SendTo (or BeginSendTo) when in broadcast mode.
    - These changes help prevent "Access denied" errors on Android when sending UDP broadcast messages.

--- src/MessagePipe.Interprocess/Workers/SocketUdpClient.cs
+++ src/MessagePipe.Interprocess/Workers/SocketUdpClient.cs
@@ -9,7 +9,6 @@ namespace MessagePipe.Interprocess.Workers
     internal sealed class SocketUdpServer : IDisposable
     {
         const int MinBuffer = 4096;
-
         readonly Socket socket;
         readonly byte[] buffer;
 
@@ -19,20 +18,15 @@ namespace MessagePipe.Interprocess.Workers
             socket.ReceiveBufferSize = bufferSize;
             buffer = new byte[Math.Max(bufferSize, MinBuffer)];
         }
+
         public static SocketUdpServer Bind(int port, int bufferSize)
         {
             var server = new SocketUdpServer(bufferSize, AddressFamily.InterNetwork, ProtocolType.Udp);
             server.socket.Bind(new IPEndPoint(IPAddress.Any, port));
             return server;
         }
+
 #if NET5_0_OR_GREATER
-        /// <summary>
-        /// create UDP socket and bind for listen.
-        /// </summary>
-        /// <param name="domainSocketPath">path to socket</param>
-        /// <param name="bufferSize">socket buffer size</param>
-        /// <exception cref="SocketException">unix domain socket not supported or socket already exists even if it is not bound</exception>
-        /// <returns>UDP server with bound socket</returns>
         public static SocketUdpServer BindUds(string domainSocketPath, int bufferSize)
         {
             var server = new SocketUdpServer(bufferSize, AddressFamily.Unix, ProtocolType.IP);
@@ -44,27 +38,17 @@ namespace MessagePipe.Interprocess.Workers
         public async UniTask<ReadOnlyMemory<byte>> ReceiveAsync(CancellationToken cancellationToken)
         {
 #if NET5_0_OR_GREATER
-            var i = await socket.ReceiveAsync(buffer, SocketFlags.None, cancellationToken).ConfigureAwait(false);
+            int i = await socket.ReceiveAsync(buffer, SocketFlags.None, cancellationToken).ConfigureAwait(false);
             return buffer.AsMemory(0, i);
 #else
             var tcs = new UniTaskCompletionSource<ReadOnlyMemory<byte>>();
-
-            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, x =>
+            socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, ar =>
             {
                 int i;
-                try
-                {
-                    i = socket.EndReceive(x);
-                }
-                catch (Exception ex)
-                {
-                    tcs.TrySetException(ex);
-                    return;
-                }
-                var r = buffer.AsMemory(0, i);
-                tcs.TrySetResult(r);
+                try { i = socket.EndReceive(ar); }
+                catch (Exception ex) { tcs.TrySetException(ex); return; }
+                tcs.TrySetResult(buffer.AsMemory(0, i));
             }, null);
-
             return await tcs.Task;
 #endif
         }
@@ -78,60 +62,84 @@ namespace MessagePipe.Interprocess.Workers
     internal sealed class SocketUdpClient : IDisposable
     {
         const int MinBuffer = 4096;
-
         readonly Socket socket;
         readonly byte[] buffer;
+        // 送信先エンドポイント。Broadcast送信の場合は、ここに保持して SendTo() で使用する
+        readonly EndPoint remoteEndPoint;
+        // ブロードキャスト送信の場合は true、通常の接続なら false
+        readonly bool useSendTo;
 
-        SocketUdpClient(int bufferSize, AddressFamily addressFamily, ProtocolType protocolType)
+        SocketUdpClient(int bufferSize, AddressFamily addressFamily, ProtocolType protocolType, EndPoint remoteEndPoint, bool useSendTo)
         {
             socket = new Socket(addressFamily, SocketType.Dgram, protocolType);
             socket.SendBufferSize = bufferSize;
             buffer = new byte[Math.Max(bufferSize, MinBuffer)];
+            this.remoteEndPoint = remoteEndPoint;
+            this.useSendTo = useSendTo;
         }
 
         public static SocketUdpClient Connect(string host, int port, int bufferSize)
         {
             var ipaddr = IPAddress.Parse(host);
-            var client = new SocketUdpClient(bufferSize, ipaddr.AddressFamily, ProtocolType.Udp);
-            client.socket.Connect(new IPEndPoint(ipaddr, port));
-            return client;
+            bool isBroadcast = ipaddr.Equals(IPAddress.Broadcast) || ipaddr.ToString() == "255.255.255.255";
+            if (isBroadcast)
+            {
+                // ブロードキャストの場合、Connect() は行わず、送信先エンドポイントを保持し、Broadcastオプションを有効にする
+                var endPoint = new IPEndPoint(ipaddr, port);
+                var client = new SocketUdpClient(bufferSize, ipaddr.AddressFamily, ProtocolType.Udp, endPoint, true);
+                client.socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+                return client;
+            }
+            else
+            {
+                // 通常はConnect() を呼び出して既定の送信先を設定
+                var endPoint = new IPEndPoint(ipaddr, port);
+                var client = new SocketUdpClient(bufferSize, ipaddr.AddressFamily, ProtocolType.Udp, endPoint, false);
+                client.socket.Connect(endPoint);
+                return client;
+            }
         }
+
 #if NET5_0_OR_GREATER
-        /// <summary>
-        /// create UDP unix domain socket client and connect to server
-        /// </summary>
-        /// <param name="domainSocketPath">path to unix domain socket</param>
-        /// <param name="bufferSize"></param>
-        /// <exception cref="SocketException">unix domain socket not supported or server does not exist</exception>
-        /// <returns>UDP unix domain socket client</returns>
         public static SocketUdpClient ConnectUds(string domainSocketPath, int bufferSize)
         {
-            var client = new SocketUdpClient(bufferSize, AddressFamily.Unix, ProtocolType.IP);
-            client.socket.Connect(new UnixDomainSocketEndPoint(domainSocketPath));
+            var endPoint = new UnixDomainSocketEndPoint(domainSocketPath);
+            var client = new SocketUdpClient(bufferSize, AddressFamily.Unix, ProtocolType.IP, endPoint, false);
+            client.socket.Connect(endPoint);
             return client;
         }
 #endif
 
-        public UniTask<int> SendAsync(byte[] buffer, CancellationToken cancellationToken = default)
+        public UniTask<int> SendAsync(byte[] data, CancellationToken cancellationToken = default)
         {
 #if NET5_0_OR_GREATER
-            return socket.SendAsync(buffer, SocketFlags.None, cancellationToken);
+            if (useSendTo)
+            {
+                // ブロードキャストの場合は SendTo を使用
+                return socket.SendToAsync(data, SocketFlags.None, remoteEndPoint, cancellationToken);
+            }
+            else
+            {
+                return socket.SendAsync(data, SocketFlags.None, cancellationToken);
+            }
 #else
             var tcs = new UniTaskCompletionSource<int>();
-            socket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, x =>
-             {
-                 int i;
-                 try
-                 {
-                     i = socket.EndSend(x);
-                 }
-                 catch (Exception ex)
-                 {
-                     tcs.TrySetException(ex);
-                     return;
-                 }
-                 tcs.TrySetResult(i);
-             }, null);
+            if (useSendTo)
+            {
+                socket.BeginSendTo(data, 0, data.Length, SocketFlags.None, remoteEndPoint, ar =>
+                {
+                    try { tcs.TrySetResult(socket.EndSend(ar)); }
+                    catch (Exception ex) { tcs.TrySetException(ex); }
+                }, null);
+            }
+            else
+            {
+                socket.BeginSend(data, 0, data.Length, SocketFlags.None, ar =>
+                {
+                    try { tcs.TrySetResult(socket.EndSend(ar)); }
+                    catch (Exception ex) { tcs.TrySetException(ex); }
+                }, null);
+            }
 #if !UNITY_2018_3_OR_NEWER
             return new UniTask<int>(tcs.Task);
 #else
